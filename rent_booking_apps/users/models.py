28491f6af13c4039212399ad7e71544273321ffc
N.Dorves <dorves@gmx.net>
2024-11-13 18:12:49 +0100

    added fields landlord and tenant for profile users,

--- rent_booking_apps/users/models.py
+++ rent_booking_apps/users/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # https://dev.to/earthcomfy/django-user-profile-3hik
 # https://github.com/earthcomfy/Django-registration-and-login-system.git
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(
-        to=User,
-        on_delete=models.CASCADE,
-        related_name='profile'
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     description = models.TextField(blank=True, null=True)
+    landlord = models.BooleanField(null=True)
+    tenant = models.BooleanField(null=True)
 
     def __str__(self):
         return self.user.username
