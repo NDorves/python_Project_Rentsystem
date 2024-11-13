fcfb154918a5d6dd38bdcfd8da320506e26d7e68
N.Dorves <dorves@gmx.net>
2024-11-13 13:46:44 +0100

    add models to project: listings, bookings, reviews, users.  add migrations.  add admins register for listings, bookings, reviews, users.

---
+++ rent_booking_apps/routers.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('rent_booking_apps.users.urls')),
+    path('', include('rent_booking_apps.bookings.urls')),
+    path('', include('rent_booking_apps.listings.urls')),
+    path('', include('rent_booking_apps.reviews.urls')),
+    path('', include('rent_booking_apps.user.urls')),
+]
